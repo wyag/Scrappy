961ff19a2c927b8a178df5719e3e533e3b213695
Isaac <isaacjgonzalez25@gmail.com>
2018-05-17 09:49:56 -0600

    Updated Home & Nav

--- Scrappy/Scrappy/Isaac/Supporting Functions/UIColorEx.swift
+++ Scrappy/Scrappy/Isaac/Supporting Functions/UIColorEx.swift
@@ -11,12 +11,12 @@ import UIKit
 extension UIColor {
     
     struct AppColorPalette {
-        static let HomeGreen = UIColor(red: 104/255, green: 217/255, blue: 155/255, alpha: 0.7)
-        static let HomePurple = UIColor(red: 181/255, green: 121/255, blue: 208/255, alpha: 0.7)
-        static let HomeLightPurple = UIColor(red: 145/255, green: 171/255, blue: 238/255, alpha: 0.7)
-        static let HomePink = UIColor(red: 1, green: 120/255, blue: 157/255, alpha: 0.7)
-        static let HomeBlue = UIColor(red: 112/255, green: 233/255, blue: 234/255, alpha: 0.7)
-        static let HomeLightGreen = UIColor(red: 170/255, green: 217/255, blue: 112/255, alpha: 0.7)
+        static let HomeGreen = UIColor(red: 104/255, green: 217/255, blue: 155/255, alpha: 1)
+        static let HomePurple = UIColor(red: 181/255, green: 121/255, blue: 208/255, alpha: 1)
+        static let HomeLightPurple = UIColor(red: 145/255, green: 171/255, blue: 238/255, alpha: 1)
+        static let HomePink = UIColor(red: 1, green: 120/255, blue: 157/255, alpha: 1)
+        static let HomeBlue = UIColor(red: 112/255, green: 233/255, blue: 234/255, alpha: 1)
+        static let HomeLightGreen = UIColor(red: 170/255, green: 217/255, blue: 112/255, alpha: 1)
     }
     
     
