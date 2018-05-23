4626240798f0625c860d7c4fb8350fb464614e47
Isaac <isaacjgonzalez25@gmail.com>
2018-05-23 15:28:13 -0600

    Updates to Checkout Page

--- Scrappy/Scrappy/Isaac/Cart/Cart VC.swift
+++ Scrappy/Scrappy/Isaac/Cart/Cart VC.swift
@@ -14,7 +14,7 @@ class CartViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //setupNav()
+        setupNav()
         setupUI()
     }
 
@@ -56,112 +56,9 @@ class CartViewController: UIViewController, UITableViewDelegate, UITableViewData
         // Setup Nav Bar
         self.navigationController?.navigationBar.isHidden = false
         self.navigationController?.navigationBar.barTintColor = UIColor.black
-        
-        // 'menuBarButton'
-        menuBarButton = UIBarButtonItem(image: UIImage(named: "hMenu"), style: .plain, target: self, action: #selector(self.menuButtonTapped(_:)))
-        menuBarButton.tintColor = UIColor.orange
-        self.navigationItem.setLeftBarButton(menuBarButton, animated: false)
-        
-        menu.closeMenuButton.addTarget(self, action: #selector(self.menuButtonTapped(_:)), for: .touchUpInside)
-        menu.homeMenuButton.addTarget(self, action: #selector(self.goToVC(_:)), for: .touchUpInside)
-        menu.birthdayMenuButton.addTarget(self, action: #selector(self.goToVC(_:)), for: .touchUpInside)
-        menu.seasonalMenuButton.addTarget(self, action: #selector(self.goToVC(_:)), for: .touchUpInside)
-        menu.holidayMenuButton.addTarget(self, action: #selector(self.goToVC(_:)), for: .touchUpInside)
-        menu.sportsMenuButton.addTarget(self, action: #selector(self.goToVC(_:)), for: .touchUpInside)
-        menu.congratsMenuButton.addTarget(self, action: #selector(self.goToVC(_:)), for: .touchUpInside)
-        menu.miscMenuButton.addTarget(self, action: #selector(self.goToVC(_:)), for: .touchUpInside)
-        menu.cartMenuButton.addTarget(self, action: #selector(self.goToVC(_:)), for: .touchUpInside)
-        menu.profileButton.addTarget(self, action: #selector(self.goToVC(_:)), for: .touchUpInside)
-    }
-    
-    
-    
-    @objc private func menuButtonTapped(_ sender: UIBarButtonItem) {
-        if !isMenuOpen {
-            self.view.addSubview(menu)
-            isMenuOpen = true
-            menu.closeMenuButton.setImage(UIImage(named: "whiteX"), for: .normal)
-            UIView.animate(withDuration: 1) {
-                self.menu.navMenuView.frame.origin.x = 0
-                self.menu.blurView.alpha = 1
-                self.menu.closeMenuButton.alpha = 1
-            }
-        } else {
-            print("Menu Closed")
-            isMenuOpen = false
-            menu.closeMenuButton.setImage(UIImage(named: "whiteX2"), for: .normal)
-            UIView.animate(withDuration: 1, animations: {
-                self.menu.blurView.alpha = 0
-                self.menu.closeMenuButton.alpha = 0
-                self.menu.navMenuView.frame.origin.x = ((self.view.frame.width/2) * -1)
-            }) { (success) in
-                if success {
-                    self.menu.removeFromSuperview()
-                }
-            }
-        }
-    }
-    
-    
-    @objc private func goToVC(_ sender: UIButton) {
-        
-        print("Nav Menu Button Pressed!!")
-        
-        // Unwrap Button ID
-        guard let id = sender.accessibilityIdentifier else { return }
-        guard let vcid = self.accessibilityValue else { return }
-        
-        // Switch On Button ID
-        switch id {
-        case vcid:
-            menuButtonTapped(self.menuBarButton)
-        case "Home":
-            self.navigationController?.popToRootViewController(animated: true)
-        case "Birthday":
-            menuButtonTapped(menuBarButton)
-            let nextVC = CollectionViewController()
-            nextVC.accessibilityValue = "Birthday"
-            self.navigationController?.show(nextVC, sender: self)
-        case "Seasonal":
-            menuButtonTapped(menuBarButton)
-            let nextVC = CollectionViewController()
-            nextVC.accessibilityValue = "Seasonal"
-            self.navigationController?.show(nextVC, sender: self)
-        case "Holiday":
-            menuButtonTapped(menuBarButton)
-            let nextVC = CollectionViewController()
-            nextVC.accessibilityValue = "Holiday"
-            self.navigationController?.show(nextVC, sender: self)
-        case "Sports":
-            menuButtonTapped(menuBarButton)
-            let nextVC = CollectionViewController()
-            nextVC.accessibilityValue = "Sports"
-            self.navigationController?.show(nextVC, sender: self)
-        case "Congrats":
-            menuButtonTapped(menuBarButton)
-            let nextVC = CollectionViewController()
-            nextVC.accessibilityValue = "Congrats"
-            self.navigationController?.show(nextVC, sender: self)
-        case "Misc":
-            menuButtonTapped(menuBarButton)
-            let nextVC = CollectionViewController()
-            nextVC.accessibilityValue = "Misc"
-            self.navigationController?.show(nextVC, sender: self)
-        case "Profile":
-            menuButtonTapped(menuBarButton)
-            let nextVC = ProfileSettingViewController()
-            nextVC.accessibilityValue = "Profile"
-            self.navigationController?.show(nextVC, sender: self)
-        case "Cart":
-            menuButtonTapped(menuBarButton)
-            let nextVC = CartViewController()
-            nextVC.accessibilityValue = "Cart"
-            self.navigationController?.show(nextVC, sender: self)
-        case "Logout":
-            self.navigationController?.popToRootViewController(animated: true)
-        default:
-            menuButtonTapped(self.menuBarButton)
-        }
+        self.navigationController?.navigationBar.tintColor = UIColor.orange
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white, NSAttributedStringKey.font: UIFont(name: "AvenirNext-Regular", size: 22) as Any]
+        self.navigationItem.title = " Cart "
     }
     
     
@@ -200,6 +97,7 @@ class CartViewController: UIViewController, UITableViewDelegate, UITableViewData
         checkoutButton.layer.shadowOffset = CGSize(width: 0, height: 1)
         checkoutButton.frame = CGRect(x: 0, y: (self.view.frame.height - 100), width: (self.view.frame.width/3 * 2), height: 60)
         checkoutButton.center.x = (self.view.center.x - 5)
+        checkoutButton.addTarget(self, action: #selector(self.goToCheckout(_:)), for: .touchUpInside)
         
         ////////// Add Subview's
         
@@ -251,6 +149,14 @@ class CartViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         // Return View
         return cartHeaderView
+    }
+    
+    
+    @objc private func goToCheckout(_ sender: UIButton) {
+        
+        let nextVC = CheckoutViewController()
+        self.navigationController?.present(nextVC, animated: true, completion: nil)
+        
     }
     
 ////////// End Of Class
