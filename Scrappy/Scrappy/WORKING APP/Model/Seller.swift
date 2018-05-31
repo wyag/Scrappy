3a3f162de1d598776abd0d39a7685e4773d5bfea
Herman Kwan <HermanK@Hermans-Macbook-Pro.local>
2018-05-31 16:29:18 -0600

    Fixed fetching user info

---
+++ Scrappy/Scrappy/WORKING APP/Model/Seller.swift
@@ -0,0 +1,22 @@
+//
+//  Seller.swift
+//  Scrappy
+//
+//  Created by Herman Kwan on 5/31/18.
+//  Copyright © 2018 Isaac. All rights reserved.
+//
+
+import UIKit
+
+class Seller {
+    
+    var name: String
+    var image: UIImage
+    
+    init(name: String, image: UIImage) {
+        
+        self.name = name
+        self.image = image
+    }
+}
+
